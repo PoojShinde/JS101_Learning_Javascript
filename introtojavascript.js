fc06dc9aadd5a1dd791c98aaf2d5a4da07dc2628
poojShinde <poojashindhe13@gmail.com>
2024-02-23 15:52:11 +0530

    intro to the javascript day 1

---
+++ introtojavascript.js
@@ -0,0 +1,10 @@
+// Write a function named addNumbers which takes two numbers as input and return their sum
+
+// Use the function to calculate the sum of given three numbers
+let a = 10;
+let b = 20;
+function addNumbers(a,b){
+  sum = a+b;
+  console.log(sum);
+}
+addNumbers(a,b);
